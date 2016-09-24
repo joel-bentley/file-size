77f05e61f08dbfbca260571cfbd98d024970b2a5
Joel Bentley <joel-bentley@users.noreply.github.com>
2016-09-24 01:03:24 +0000

    add form and post route

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
-var multer = require('multer');
+var multer = require('multer')({
+    limits: {
+        fileSize: 1e6
+    },
+});
+
 var app = require('express')();
 
 app.set('views', './views');
@@ -7,8 +12,13 @@ app.set('view engine', 'pug');
 var port = process.env.PORT || 8080;
 
 app.get('/', function(req, res) {
-
     res.render('index');
+});
+
+app.post('/upload', multer.single('file'), function(req, res) {
+    res.json({
+        size: req.file.size
+    });
 });
 
 app.listen(port, function() {
